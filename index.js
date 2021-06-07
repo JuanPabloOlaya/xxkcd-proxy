ed834e479f36aff1b7e3dcd8d5f517c9d7a63e12
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2021-06-07 18:13:07 -0500

    Testing params

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page'));
+app.get('/:comicId', async (req, res) => {
+  const { comicId } = req.params;
+  let response = {};
+
+  if (comicId === 'latest')  {
+    response = await axios('https://xkcd.com/info.0.json');
+  } else {
+    response = await axios(`https://xkcd.com/${comicId}/info.0.json`);
+  }
+
+  res.send(response.data);
+});
 
 const port = process.env.PORT || 3000;
 
