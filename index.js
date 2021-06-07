b9fe9b025abb5b21b70886ecd2a3858401ccabed
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2021-06-07 15:42:07 -0500

    Probando

--- index.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-module.exports = async (req, res) => {
-  const response = await fetch('https://xkcd.com/info.0.json');
-  const json = await response.json();
-
-  return json;
-}
