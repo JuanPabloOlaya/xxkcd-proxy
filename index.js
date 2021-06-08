266ba1c0723aed5099a05f1f1632050bb5c64012
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2021-06-07 20:35:55 -0500

    Agregando CORS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
+
+app.use(cors());
 
 app.get('/:comicId', async (req, res) => {
   const { comicId } = req.params;
